ed79b1ceaf57ae81793fa312761766a8fc614eec
Quoc Thai <quocthai2000x@gmail.com>
2022-12-18 02:45:03 +0700

    update netlify

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(sessions({
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["http://localhost:5173", "https://project-iii-front.vercel.app"],
+    origin: ["http://localhost:5173", "https://project-iii-front.vercel.app", "https://project-iii-front.netlify.app"],
     credentials: true
 }
 ))
