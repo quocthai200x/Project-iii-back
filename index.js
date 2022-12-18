4b3b4e5e8b0d673d7837d6aaddc6f214eaf2fa04
Quoc Thai <quocthai2000x@gmail.com>
2022-12-18 17:12:40 +0700

    dm

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ var router = require("./application/router")
 var path = require("path")
 const port = process.env.port || 6969;
 
+console.log(process.env.NODE_ENV)
+
+console.log()
+
 app.use(sessions({
     cookieName: "session",
    
@@ -18,17 +22,16 @@ app.use(sessions({
     duration: process.env.MY_IMPOSSIIBLE_SECRET,
     activeDuration: parseInt((new Date()).getTime() / 1000, 10),
     cookie: {
-        secureProxy: process.env.NODE_ENV === 'development'? false : true,
+        secureProxy: process.env.NODE_ENV.trim() === 'development' ? false : true,
         httpOnly: false,
         ephemeral: false,
-        secure: process.env.NODE_ENV === 'development'? false : true,
-        sameSite: process.env.NODE_ENV === 'development'?true : "none",
+        secure: process.env.NODE_ENV.trim() === 'development' ? false : true,
+        sameSite:process.env.NODE_ENV.trim() === 'development'?true : "none",
     }
 }));
 
-
 app.use((req, res, next)=>{
-    if(!(process.env.NODE_ENV === 'development')){
+    if(!(process.env.NODE_ENV.trim() === 'development')){
         app.set('trust proxy', true)
         req["session"].secure = true;
         req['session'].secureProxy = true
